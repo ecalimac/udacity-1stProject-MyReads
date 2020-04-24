76853c66140d4a1f18307d0e6b3e61bc20475a8f
ecalimac <elena.calimac@gmail.com>
2020-04-24 14:42:06 +0000

    use API to get the books and populate the state

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import React from 'react'
 import { Route } from 'react-router-dom'
-// import * as BooksAPI from './BooksAPI'
 import BookShelves from './BookShelves'
 import SearchBook from './SearchBook'
+import * as BooksAPI from './BooksAPI'
 import './App.css'
 
 class BooksApp extends React.Component {
   state = {
+    books: []
   }
 
+  componentDidMount() {
+    // Get the books from BooksAPI and populate state
+    BooksAPI.getAll()
+	.then(data => {
+      this.setState({ books: data });
+      //console.log(this.state);
+    });
+  }
   render() {
     return (
       <div className="app">
