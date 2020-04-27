99457ca45f25f0063a91f9344f9e3a62c8be956b
ecalimac <elena.calimac@gmail.com>
2020-04-27 12:24:46 +0000

    updateBooks function, BookShelves.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import { Route } from 'react-router-dom'
+import * as BooksAPI from './BooksAPI'
 import BookShelves from './BookShelves'
 import SearchBook from './SearchBook'
-import * as BooksAPI from './BooksAPI'
 import './App.css'
 
 class BooksApp extends React.Component {
@@ -18,11 +18,24 @@ class BooksApp extends React.Component {
       //console.log(this.state);
     });
   }
+  updateShelf = (book, shelf) => {
+  	BooksAPI.update(book, shelf).then(() => {
+    	this.setState(prevState=>({
+        	books: prevState.books.filter(b => {
+            	if(b.id === book.id) {
+                	return (book.shelf = shelf);
+                } else {
+                	return book;
+                }
+            })
+        }));
+    });
+  };
 
   render() {
     return (
       <div className="app">
-		<Route exact path='/' component={BookShelves}/>
+		<Route exact path='/' render={()=>(<BookShelves books={this.state.books} update={this.updateShelf}/>)}/>
     	<Route path='/search' render={()=>(
         	<SearchBook component={SearchBook}/>
         )}/>
---
+++ src/ListBooks.js
@@ -0,0 +1,52 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+
+class ListBooks extends Component {
+  render(){
+    const {books, update} = this.props;
+    return(
+      <ol className="books-grid">
+      {books.map(eachBook => (
+      	<li key={eachBook.id}>
+        	<div className="book">
+            	<div className="book-top">
+                	{/* if book have imageLinks display the thumbnail */}
+					{eachBook.imageLinks && (
+                    <div className="book-cover" style={{ width: 128, height: 193, backgroundImage:`url(${eachBook.imageLinks.thumbnail})`}}></div>)}
+					{/* if book doesn't habe imageLinks display a placeholder image */}
+					{!eachBook.imageLinks && (
+                    <div className="book-cover" style={{ width: 128, height: 193, backgroundColor: "light-grey"}}></div>
+                    )}
+                    <div className="book-shelf-changer">
+                    	<select
+							id={eachBook.id}
+							value={eachBook.shelf?eachBook.Shelf : "None"}
+							onChange={event => update(eachBook, event.target.value)}>
+                        	<option value="move" disabled>Move to...</option>
+                            <option value="currentlyReading">Currently Reading</option>
+                            <option value="wantToRead">Want to Read</option>
+                            <option value="read">Read</option>
+                            <option value="none">None</option>
+                        </select>
+                    </div>
+				</div>
+				<div className="book-title">{eachBook.title}</div>
+             	{eachBook.authors &&
+                eachBook.authors.map(author => (
+                <div className="book-authors" key={author}>
+                    {author}
+                </div>
+                ))}
+             	</div>
+        	</li>
+            ))}
+		</ol>
+    );
+  }
+}
+ListBooks.propTypes = {
+	books: PropTypes.array.isRequired,
+  	update: PropTypes.func.isRequired
+}
+
+export default ListBooks
